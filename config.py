af430695a970b0b5079d74a08cc05916046029c2
Vaishnavi Asawale <vaishnaviasawale@gmail.com>
2026-07-08 22:38:36 +0530

    Placeholder functions to enable disable the running app

---
+++ config.py
@@ -0,0 +1,21 @@
+import json
+from pathlib import Path
+
+CONFIG_DIR = Path.home() / ".config" / "break-reminder"
+
+CONFIG_FILE = CONFIG_DIR / "config.json"
+
+def ensure_config_exists():
+    pass
+
+def load_config():
+    pass
+
+def save_config(config):
+    pass
+
+def reminders_enabled():
+    pass
+
+def set_enabled(enabled):
+    pass
